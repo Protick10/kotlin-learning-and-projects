003d26876b18f54a21b6937a0a143e5ef2b4896e
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-05-09 02:55:38 +0600

    practice basic variable

--- secondday/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ secondday/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -4,4 +4,17 @@ fun main(){
     println("Hello World!")
     println("i am learning kotlin")
 
+    val a = 10 // val is used to declare a variable which is immutable (read-only) and cannot be changed once assigned.
+    var b = 20 // var is used to declare a variable which is mutable and can be changed later in the program.
+
+    println("a = $a")
+
+    println("b = $b") // String interpolation
+    b = 30
+    println("the changed value of b = $b")
+
+    val c =10.5
+
+    println(c)
+
 }
---
+++ secondday/app/src/main/java/com/example/kotlinbasics/VariableTypes.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    val constantNumber = 100
+
+    var mutableString = "protick"
+
+    println(constantNumber)
+    println(mutableString)
+
+   mutableString = "anik"
+
+   println(mutableString)
+
+}
