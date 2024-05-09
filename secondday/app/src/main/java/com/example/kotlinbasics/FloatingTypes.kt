cc6aa6914fcc674dbfd8308fe5c063ffcefa683c
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-05-10 00:55:18 +0600

    newfile

---
+++ secondday/app/src/main/java/com/example/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+
+
+fun main(){
+    var circleRatio = 3.14
+
+    println(circleRatio)
+
+   var circleRatio2 = 3.14f
+
+    println(circleRatio2)
+
+    circleRatio = 3.116596535
+}
+
+
