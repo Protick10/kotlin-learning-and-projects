0bc259d4afbcddb2a167f17eb0cae3da68763cf5
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-05-24 22:32:38 +0600

    FORTH DAY STARTED

---
+++ app/src/main/java/pro/inc/fourthday/list.kt
@@ -0,0 +1,30 @@
+package pro.inc.fourthday
+
+fun main(){
+
+
+    //immutable list
+
+    val shoppingList = listOf("Processor", "Motherboard", "RAM", "Graphics Card", "Power Supply", "Storage")
+
+    //mutable list
+
+    val shoppingListMutable = mutableListOf("Processor", "Motherboard", "RAM", "Graphics Card", "Power Supply", "Storage")
+
+    //add item to mutable list
+    shoppingListMutable.add("Cooling Fan")
+    shoppingListMutable.remove("Cooling Fan")
+
+    println(shoppingListMutable)
+
+    shoppingListMutable.set(5, "SSD")
+    println(shoppingListMutable)
+
+    val hasram = shoppingListMutable.contains("RAM")
+
+    if (hasram){
+        println("You have RAM in your shopping list")}
+    else{
+        println("You don't have RAM in your shopping list")
+    }
+}
