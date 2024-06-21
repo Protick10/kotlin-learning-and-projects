b01b7ae122336a4bf739a38dad050a6374d0e3ed
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-06-22 00:36:36 +0600

    counter without mvvm completed

--- counter app using mvvm day 8/app/src/main/java/pro/inc/counterappusingmvvm/CounterViewModel.kt
+++ counter app using mvvm day 8/app/src/main/java/pro/inc/counterappusingmvvm/CounterViewModel.kt
@@ -1,19 +1,79 @@
 package pro.inc.counterappusingmvvm
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.room.util.copy
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
-class CounterViewModel : ViewModel() {
-    private var _count = mutableStateOf(0)
-    public var count = _count
 
-    fun increment() {
-        _count.value += 1
+@Composable
+fun CounterViewModel(){
+    var count by remember { mutableStateOf(0) }
+
+    fun increment(){
+        count++
     }
 
-    fun decrement() {
-        _count.value -= 1
+    fun decrement(){
+        count--
     }
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Count: $count",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+            color = Color.Black,
+            style = MaterialTheme.typography.headlineLarge,
+            )
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = { increment() },
+                modifier = Modifier.padding(8.dp),
+
+            ) {
+                Text(text = "Increment")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = { decrement() },
+                modifier = Modifier.padding(8.dp),
+
+                ) {
+                Text(text = "Decrement")
+            }
+
+        }
+
+
+
+
+    }
 }
--- counter app using mvvm day 8/app/src/main/java/pro/inc/counterappusingmvvm/MainActivity.kt
+++ counter app using mvvm day 8/app/src/main/java/pro/inc/counterappusingmvvm/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import pro.inc.counterappusingmvvm.ui.theme.CounterAppUsingMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CounterAppUsingMVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+
+                ) {
+                    CounterViewModel()
                 }
             }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterAppUsingMVVMTheme {
-        Greeting("Android")
-    }
-}
