a2a153de15dbc28959d3f6e3f91238cf4f9a5048
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-06-22 23:59:14 +0600

    day 8 finished mvvm finished

--- counter app with MVVM/app/src/main/java/pro/inc/counterwithmvvm/CounterViewModel.kt
+++ counter app with MVVM/app/src/main/java/pro/inc/counterwithmvvm/CounterViewModel.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModel : ViewModel(){
-    private val _count = mutableStateOf(0) // Mutable state to hold the count value this is private to avoid direct access. only the view model can access it
-
+class CounterViewModel() : ViewModel(){
+//    private val _count = mutableStateOf(0) // Mutable state to hold the count value this is private to avoid direct access. only the view model can access it
+    private val _repository: CounterRepository =  CounterRepository() // Repository to hold the counter model
+    private val _count = mutableStateOf(_repository.getCounter().count) // use the repository of countermodel to get the initial value of the count
     val count: MutableState<Int> = _count  // Exposing the mutable state as immutable state to the view
 
     fun increment(){
-        _count.value++
+        _repository.incrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
     fun decrement(){
-        _count.value--
+        _repository.decrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
 }
