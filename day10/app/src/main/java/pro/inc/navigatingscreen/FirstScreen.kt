5de37c598c8b6f1d5a80c97c00eb2cd67e0841d6
Protick Saha <85408091+Protick10@users.noreply.github.com>
2024-06-30 19:52:20 +0600

    day 10

---
+++ day10/app/src/main/java/pro/inc/navigatingscreen/FirstScreen.kt
@@ -0,0 +1,58 @@
+package pro.inc.navigatingscreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pro.inc.navigatingscreen.ui.theme.NavigatingScreenTheme
+
+@Composable
+
+fun FirstScreen() {
+    var name = remember {
+        mutableStateOf("")
+
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "This is the first screen",
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        OutlinedTextField(value = name.value, onValueChange = {
+            name.value = it
+        },
+            label = { Text("Enter your name") },
+            modifier = Modifier.padding(16.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text ="Navigate to second screen",
+                modifier = Modifier.padding(16.dp))
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavigatingScreenTheme {
+        FirstScreen()
+    }
+}
